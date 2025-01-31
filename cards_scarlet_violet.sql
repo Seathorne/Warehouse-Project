b1b3ea56d50869c8b804a58e1e5a0f69e80fdf90
Seathorne <scottketclarke@gmail.com>
2025-01-31 17:52:12 -0500

    Attempt to generate unique card_id
    Set up database with new cards (though cards have NULL card_id by DEFAULT).
    
    Final query not working yet.

--- cards_scarlet_violet.sql
+++ cards_scarlet_violet.sql
@@ -111,3 +111,76 @@ WHERE card_id > (
                        WHERE poke_id = 137
 )
 AND poke_id = 137;
+
+---------------------------------------------------
+/*  6. Insert new cards from Scarlet & Violet.  */
+---------------------------------------------------
+-- Add new columns to Cards { poke_type, poke_hp }:
+--  (Separate statements for using SQLite.)
+ALTER TABLE Cards
+    ADD COLUMN poke_type INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE Cards
+    ADD COLUMN poke_hp INTEGER NOT NULL DEFAULT 0;
+
+-- Modify column set_id to TEXT instead of INTEGER
+--  and allow NULL as default.
+/* (Not supported in SQLite)
+ * ALTER TABLE Cards
+ * MODIFY COLUMN set_id VARCHAR(20) DEFAULT NULL;
+ */
+
+-- Delete all new cards from sets SPE, OBF, TEF, and SSP:
+DELETE FROM Cards
+WHERE set_id IN ('SPE', 'OBF', 'TEF', 'SSP');
+
+-- Insert new cards from sets { SPE, OBF, TEF, and SSP }:
+WITH New_Cards(card_name, poke_id, poke_type, poke_hp, set_id, set_no, illustrator, rarity_class) AS (
+    SELECT 'Phanpy', 231, 'Fighting', 80, 'SPE', 205, 'Saboteri', 'Illustration Rare'
+    UNION ALL
+    SELECT 'Phantump', 708, 'Grass', 60, 'OBF', 11, 'Narumi Sato', 'Common'
+    UNION ALL
+    SELECT 'Roselia', 315, 'Grass', 60, 'TEF', 8, 'Tomomi Ozaki', 'Common'
+    UNION ALL
+    SELECT 'Iron Crown', 1023, 'Metal', 130, 'SSP', 132, 'akagi', 'Holo Rare'
+)
+INSERT INTO Cards (card_name, poke_id, poke_type, poke_hp, set_id, set_no, illustrator, rarity_class)
+    SELECT * FROM New_Cards N
+    WHERE NOT EXISTS (
+        SELECT 1 FROM Cards C
+        WHERE C.set_id = N.set_id AND C.set_no = N.set_no
+);
+
+-- Show all new cards
+    /* TODO: Need to add poke_hp and poke_type columns  */
+SELECT card_id, card_name, poke_id, set_id, set_no, illustrator, rarity_class
+FROM Cards
+WHERE set_id IN ('SPE', 'OBF', 'TEF', 'SSP');
+
+-- Show all cards with NULL card_id:
+SELECT card_id, card_name, poke_id, set_id, set_no, illustrator, rarity_class
+FROM Cards
+WHERE card_id IS NULL;
+
+/* Query that performs the following:
+    (A) updates all null ID values, by
+    (B) creating an intermediate value of set_id and set_no,
+    (C) sorting by that intermediate value,
+    and then (D) adding 1 to the highest existing ID value.
+ */
+UPDATE Cards -- (A. update card_id values)
+SET card_id = (
+    SELECT max_id + row_num -- (D. add row_num to max_id)
+    FROM (
+        SELECT C.card_id,
+        (SELECT MAX(card_id) FROM Cards
+                             WHERE card_id IS NOT NULL
+        ) AS max_id, -- (B. get max_id)
+        ROW_NUMBER() OVER (
+            ORDER BY (set_id * 1000 + set_no)
+        ) AS row_num -- (C. order by set_id, set_no)
+       FROM Cards C
+       WHERE C.card_id IS NULL
+   ) numbered_sets
+   WHERE numbered_sets.card_id = Cards.card_id
+)
+WHERE card_id IS NULL; -- (A. update only if null)
