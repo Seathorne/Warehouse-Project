9d4c7ff1aa5eaeab1188f79bd510f59161101997
Seathorne <scottketclarke@gmail.com>
2025-01-31 23:29:14 -0500

    Update basicQueries.sql
    Add alternate syntax support for row- limited searches.

--- basicQueries.sql
+++ basicQueries.sql
@@ -1,17 +1,34 @@
+/* RETRIEVING DATA PRACTICE ******
+   Retrieving single/multiple columns
+   Retrieving all columns
+   Retrieving distinct rows
+   Retrieving with limited access
+   */
 -- Select all:
 SELECT * FROM gen1Pokemon;
 
--- Select distinct types:
+-- Select all distinct types:
 SELECT DISTINCT Type FROM gen1Pokemon;
 
--- Select 5 count:
+-- Select at most 5 rows:
 SELECT * FROM gen1Pokemon
 LIMIT 5;
 
--- Starting at 10, select 5 count:
+-- Starting at 10, select at most 5 rows:
 SELECT * FROM gen1Pokemon
 LIMIT 10, 5;
 
--- Cut off at 3 when reaching end of table:
+-- Select is cut off at end of table
 SELECT * FROM gen1Pokemon
-LIMIT 15, 5
+LIMIT 15, 5;
+
+-- Alternate syntax for limiting:
+SELECT 5 FROM gen1Pokemon;
+
+-- Syntax where limiting not supported:
+WITH ordered AS
+(
+ SELECT *, row_number() OVER (ORDER BY Number) as row_num
+ FROM gen1Pokemon
+)
+SELECT * FROM ordered WHERE row_num >= 15;
