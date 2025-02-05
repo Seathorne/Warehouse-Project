4b052e150f6ebba2a94442172a7337a45cd27306
Seathorne <scottketclarke@gmail.com>
2025-02-04 22:59:55 -0500

    Update tcgDex.sql

--- tcgDex.sql
+++ tcgDex.sql
@@ -5,11 +5,13 @@ CREATE TABLE tcgPokeDex (
   Name,
   Description TEXT(50) NULL,
   Type1 TEXT(10),
-  Type2 TEXT(10)
+  Type2 TEXT(10),
+  Generation INTEGER,
+  Region TEXT(20)
 );
 
 INSERT INTO tcgPokeDex
-SELECT PokeNumber, CardName, NULL, Type, DeltaType
+SELECT PokeNumber, CardName, NULL, Type, DeltaType, NULL, NULL
 FROM tcgCards2;
 
 SELECT * FROM tcgPokeDex;
